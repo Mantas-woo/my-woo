c53eb0f6349464ce4c5ec60e4ffd907237e5c1c4
Mantas-eng <68243914+Mantas-eng@users.noreply.github.com>
2025-09-24 22:54:39 +0300

    Deploy commit with GraphQL WooCommerce

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   /** * Depending on your servers capabilities, you may need to adjust the following settings. * It will affect the build time but also increase the reliability of the build process. * If you have a server with a lot of memory and CPU, you can remove the following settings. * @property {number} concurrency - How many pages to prerender at once * @property {number} interval - How long to wait between prerendering pages * @property {boolean} failOnError - This stops the build from failing but the page will not be statically generated */
 
   nitro: {
+    preset: "vercel",
     prerender: {
       concurrency: 10,
       interval: 1000,
